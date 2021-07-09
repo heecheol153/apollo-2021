c43b736938b2ddd4cdb8839b3bcea9d0de7f50f5
heecheol153 <heecheol153@gmail.com>
2021-07-09 19:35:42 +0900

    #0.4 Router and Styles

---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+/*export default () => <h1>Home</h1>;*/
+export default class Home extends React.Component {
+  render() {
+    return (
+      <h1>Home</h1>
+    );
+  }
+}
